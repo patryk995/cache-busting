dea1038712e9c5c95b4f3730a8853c9544d19889
Patryk Bartosevic <patryk.bartosevic@horiondigital.com>
2023-06-13 21:23:13 +0300

    display version

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import packageJson from '../package.json';
 
 function App() {
+  const version = packageJson.version;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>CACHE BUSTING TEST APP</p>
+        <p>Version: {version}</p>
       </header>
     </div>
   );
